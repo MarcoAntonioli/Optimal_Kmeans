f9b477bc6b6b27bdfdb2e01ab2290084adfcc97f
Marco Antonioli <38880562+MarcoAntonioli@users.noreply.github.com>
2023-05-07 11:07:26 -0400

    Commented code and added otpimized version of distances

--- code/src/opt.jl
+++ code/src/opt.jl
@@ -1,29 +1,39 @@
 using Gurobi, JuMP
 
 function gamma_formulation(data, K)
+    # Get the number of data points and dimensions in the data
     N = size(data, 1)
     D = size(data, 2)
 
     model = Model(Gurobi.Optimizer)
     set_optimizer_attribute(model, "OutputFlag", 0)
 
-    @variable(model, a[1:N, 1:K], Bin)
-    @variable(model, f[1:N, 1:N, 1:K] >= 0)
-    @variable(model, b[N-K+1, 1:K], Bin)
-    @variable(model, γ[1:N, 1:N, 1:K, 1:N-K+1] >= 0)
+    #--------------Decision Variables----------------#
 
-    @objective(model, Min, sum(sum(sum(norm(sum((1/l) * (sum(γ[i,j,k,l]*(x[i,d] - x[j,d]) for j=1:N)) for l=1:N-K+1))^2 for k=1:K) for d=1:D) for i=1:N))
+    @variable(model, a[1:N, 1:K], Bin) # Binary indicator variable for assigning data points to clusters
+    @variable(model, f[1:N, 1:N, 1:K] >= 0) # Auxiliary variable used to linearize
+    @variable(model, b[N-K+1, 1:K], Bin) # Auxiliary variable used to linearize
+    @variable(model, γ[1:N, 1:N, 1:K, 1:N-K+1] >= 0) # Auxiliary variable used to linearize
 
-    @constraint(model, [i=1:N], sum(a[i,k] for k=1:K) == 1)
-    @constraint(model, [k=1:K], sum(a[i,k] for i=1:N) >= 1)
-    @constraint(model, [i=1:N, j=1:N, k=1:K], f[i,j,k] <= a[i,k])
+    #--------------Objective Function----------------#
+
+    # Objective function to minimize the sum of the squared Euclidean distances between data points and their assigned cluster centers
+    @objective(model, Min, sum(sum(sum(norm(sum((1/l) * (sum(γ[i,j,k,l]*(data[i,d] - data[j,d]) for j=1:N)) for l=1:N-K+1))^2 for k=1:K) for d=1:D) for i=1:N))
+
+    #----------------Constraints---------------------#
+
+    @constraint(model, [i=1:N], sum(a[i,k] for k=1:K) == 1) # Each data point is assigned to exactly one cluster
+    @constraint(model, [k=1:K], sum(a[i,k] for i=1:N) >= 1) # Each cluster has at least one data point assigned to it
+    
+    # f[i,j,k] is equal to the product a[i,k]*a[j,k]
+    @constraint(model, [i=1:N, j=1:N, k=1:K], f[i,j,k] <= a[i,k]) 
     @constraint(model, [i=1:N, j=1:N, k=1:K], f[i,j,k] <= a[j,k])
     @constraint(model, [i=1:N, j=1:N, k=1:K], f[i,j,k] >= a[i,k] + a[j,k] - 1)
+
+    # γ[i,j,k,l] is equal to the product b[l,k] * f[i,j,k]
     @constraint(model, [i=1:N, j=1:N, k=1:K, l=1:N-K+1], γ[i,j,k,l] <= f[i,j,k])
     @constraint(model, [i=1:N, j=1:N, k=1:K, l=1:N-K+1], γ[i,j,k,l] <= b[l,k])
     @constraint(model, [i=1:N, j=1:N, k=1:K, l=1:N-K+1], γ[i,j,k,l] >= f[i,j,k] + b[l,k] - 1)
 
     optimize!(model)
-
     return value.(a)
-end
--- code/src/utils.jl
+++ code/src/utils.jl
@@ -1,51 +1,160 @@
 using CSV, Tables, LinearAlgebra, Random, Gurobi, JuMP, DataFrames, Statistics, MLJ, Plots, Clustering, Distances
 
-"""
-N is the number of points in the dataset
-K is the number of clusters
-D is the dimensionality of the points
-"""
+function generate_points(N, K, D, std, seed = 42)
+    """
+    Generate a random dataset of N points with K clusters of dimensionality D.
 
-function generate_points(N, K, D, std, seed = 1234)
-    Random.seed!(seed);
+    N: number of points in the dataset
+    K: number of clusters
+    D: dimensionality of the points
+    std: standard deviation of each cluster
+    seed: random seed (optional)
+    """
+    Random.seed!(seed)
     X, yy = make_blobs(N, D; centers=K, cluster_std=std)
-    points = Matrix(DataFrame(X));
-    min = minimum(points, dims=1);
-    max = maximum(points, dims=1);
-    points = (points .- min) ./ (max .- min);
+    
+    points = Matrix(DataFrame(X))
+    min = minimum(points, dims=1)
+    max = maximum(points, dims=1)
+    points = (points .- min) ./ (max .- min)
+    
     return points
 end
 
 
 function mean_silhouette_score(assignment, counts, points)
+    """
+    Compute the mean silhouette score of a clustering assignment.
+
+    assignment: a vector of length N containing the cluster assignment of each point
+    counts: a vector of length K containing the number of points in each cluster
+    points: a matrix of size (D,N) containing the points in the dataset
+    """
     distances = pairwise(Euclidean(), points')
+    
     return mean(silhouettes(assignment, counts, distances))
 end
 
-function euclidean_distance(points)
-    n,m = size(points)
-    distances = ones((n,n))
-    for i in 1:n
-        for j in 1:n
+
+
+function euclidean_distance_1(points)
+    """
+    Compute the pairwise Euclidean distance between all points in the dataset.
+
+    points: a matrix of size (N,D) containing the points in the dataset
+    """
+    N = size(points,1)
+    distances = ones((N,N))
+    
+    for i in 1:N
+        for j in 1:N
             distances[i,j] = sqrt(sum((points[i,:] - points[j,:]).^2))
         end
     end
+    
+    return distances^2
+end
+
+function euclidean_distance_2(points)
+    """
+    Compute the pairwise Euclidean distance between all points in the dataset.
+
+    points: a matrix of size (N,D) containing the points in the dataset
+    """
+    N = size(points, 1)
+    distances = zeros(N,N)
+
+    # Compute upper triangle of distance matrix
+    for i in 1:N-1
+        for j in i+1:N
+            distances[i,j] = sqrt(sum((points[i,:] .- points[j,:]).^2))
+        end
+    end
+
+    # Copy upper triangle to lower triangle
+    distances = distances + distances'
+
+    return distances.^2
+end
+
+function euclidean_distance_3(points)
+    """
+    Compute the pairwise Euclidean distance between all points in the dataset.
+
+    points: a matrix of size (N,D) containing the points in the dataset
+    """
+    N = size(points, 1)
+    distances = sqrt.(sum(abs2.(points'), dims=1)' .+ sum(abs2.(points'), dims=1) .- 2 .* (points * points'))
+
+    return distances.^2
+end
+
+
+function manhattan_distance_1(points)
+    """
+    Compute the pairwise Manhattan distance between all points in the dataset.
+
+    points: a matrix of size (N,D) containing the points in the dataset
+    """
+    N = size(points, 1)
+    distances = ones((N,N))
+
+    for i in 1:N
+        for j in 1:N
+            distances[i,j] = sum(abs.(points[i,:] - points[j,:]))
+        end
+end
+
+function manhattan_distance_2(points)
+    """
+    Compute the pairwise Manhattan distance between all points in the dataset.
+
+    points: a matrix of size (N,D) containing the points in the dataset
+    """
+    N = size(points, 1)
+    distances = zeros(N,N)
+
+    # Compute upper triangle of distance matrix
+    for i in 1:N-1
+        for j in i+1:N
+            distances[i,j] = sum(abs.(points[i,:] .- points[j,:]))
+        end
+    end
+
+    # Copy upper triangle to lower triangle
+    distances = distances + distances'
+
     return distances
 end
 
 function manhattan_distance(points)
-    n,m = size(points)
-    distances = ones((n,n))
-    for i in 1:n
-        for j in 1:n
-            distances[i,j] = sum(abs.(points[i,:] - points[j,:]))
-        end
-    end
-    return distances
+    """
+    Compute the pairwise Manhattan distance between all points in the dataset.
+
+    points: a matrix of size (N,D) containing the points in the dataset
+    """
+    N = size(points, 1)
+    X = reshape(repeat(points, outer = (N, 1)), N, N, :)
+    Y = reshape(repeat(points', inner = (N, 1)), N, N, :)
+    return sum(abs.(X .- Y), dims = 3)
 end
 
 
+
+
+    return distances
+end
+
 function get_centroids(assignments, data)
+    """
+    Compute the centroids of the clusters given a clustering assignment and the data.
+
+    assignments: a matrix of size (N,K) containing the cluster assignment of each point
+    data: a matrix of size (N,D) containing the points in the dataset
+    """
+    K = size(assignments, 2)
+    N, D = size(data)
+
     centroids = zeros(K, D)
     for k=1:K
         centroids[k,:] = sum(assignments[i,k] * data[i,:] for i=1:N) / sum(assignments[i,k] for i=1:N)
